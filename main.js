5e5ccd9ddd421d7cfef77ec3619935ada4bcde1a
Azmir Abdulla <princeabdulla006@gmail.com>
2025-08-28 02:12:05 +0600

    heart number icrease by click on heart icon uploaded

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+
+ 
+document.getElementById("heart").addEventListener("click",function(){
+ const heartNumber =document.getElementById("heart-number");
+     let heartCount=parseInt(heartNumber.innerText);
+    for(let i=0; i < 1; i++){
+          heartCount++;
+    }
+    heartNumber.innerText = heartCount;
+  
+})
+
+
+
+
